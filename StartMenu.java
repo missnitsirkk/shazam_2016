18c637d7c5109a35940c70fb98ee91ab414bb4f8
Miss Nitsirkk <missnitsirkk11@gmail.com>
2016-05-12 13:32:21 -0500

    Update StartMenu.java

--- StartMenu.java
+++ StartMenu.java
@@ -17,10 +17,31 @@ public class StartMenu {
 		shazamData.clear();
 		InstructionMemory shazamInstructions = new InstructionMemory();
 		shazamInstructions.clear();
+		CPU shazam = new CPU();
+		String loc = "000";
+		String B = String.format("%03X", shazam.getbRegister());
+		String T = String.format("%03X", shazam.gettRegister());
+		int[] instruction = shazamInstructions.getInstructions();
+		
+		
+		boolean trace = true;
+		
+		try {
+			PrintWriter traceOutput = new PrintWriter("TraceOutput.txt", "UTF-8");
+			traceOutput.println("Interpreter --- Begin location " + loc + " B = " + B + " T = "+ T);
+			traceOutput.println("Trace is... On");
+			traceOutput.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Log Trace Failed.");
+			ex.printStackTrace();
+		} catch (UnsupportedEncodingException ex) {
+			System.out.println("Log Trace Failed.");
+			ex.printStackTrace();
+		}
 		
 		//Welcome, Choose Option
 		System.out.println("SHAZAM 2016\n\nChoose an Option by Number:");
-		System.out.println("\n1 Clear \n2 Dump\n3 Load\n0 Quit");
+		System.out.println("\n1 Clear \n2 Dump\n3 Load\n4 Interpret\n0 Quit");
 		Scanner scnr = new Scanner(System.in);
 		int input = scnr.nextInt();
 	
@@ -43,6 +64,11 @@ public class StartMenu {
 			shazamInstructions.load();
 			System.out.println("Your Input File has been loaded.\n");
 			input = scnr.nextInt();
+			break;
+		case 4: 
+			for (int i = 0; i < 0x400; i++) {
+				shazam.setiRegister(shazamInstructions.getInstruction(i));
+			}
 			break;
 		default: 
 			System.out.println("Invalid Command.\n");
